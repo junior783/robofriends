f0f5d1864af7fd947bc58e6accce16a7f10de9a9
Andre Brooks <andrejr.83@gmail.com>
2019-11-23 00:25:59 -0500

    Checkpoint

---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll';
+import SearchBox from '../components/SearchBox';
+import { robots } from '../components/robots';
+import ErrorBoundary from '../components/ErrorBoundary';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ""
+        }
+    }
+    componentDidMount(){
+        fetch("https://jsonplaceholder.typicode.com/users").then(response => {
+            return response.json();
+        }).then(users => {
+            this.setState({robots: users});
+        })
+    }
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        if(this.state.robots.length < 1)
+        {
+            return <h1>Loading</h1>;
+        }
+        else
+        {
+            return  <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll><ErrorBoundary><CardList robots={filteredRobots} /></ErrorBoundary></Scroll>
+            </div>;
+        }
+    } 
+}
+
+export default App;
