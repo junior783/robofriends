aae7726958841ec128b98e3a4b5096c09cca17f1
Andre Brooks <andrejr.83@gmail.com>
2020-04-01 22:58:59 -0400

    Testing - Part 2

---
+++ src/actions.test.js
@@ -0,0 +1,32 @@
+import * as actions from './actions';
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+} from './constants.js';
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+
+const mockStore = configureMockStore([thunkMiddleware]);
+
+it('should create and action to search robots', () => {
+    const text = 'wooo';
+    const expectedAction = {
+        type: CHANGE_SEARCH_FIELD,
+        payload: text
+    };
+
+    expect(actions.setSearchField(text)).toEqual(expectedAction);
+})
+
+it('handles requesting robots API', () => {
+    const store = mockStore();
+    store.dispatch(actions.requestRobots());
+    const actions2 = store.getActions();
+    const expectedAction = {
+        type: REQUEST_ROBOTS_PENDING
+    };
+
+    expect(actions2[0]).toEqual(expectedAction);
+})
---
+++ src/components/MainPage.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll';
+import SearchBox from '../components/SearchBox';
+import ErrorBoundary from '../components/ErrorBoundary';
+import Header from '../components/Header';
+import './MainPage.css';
+
+class MainPage extends Component {
+    componentDidMount() {
+        this.props.onRequestRobots();
+    }
+    filterRobots = () => {
+        return this.props.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.props.searchField.toLowerCase());
+        });
+    }
+    render() {
+        const { onSearchChange, robots, isPending } = this.props;
+        if (isPending) {
+            return <h1>Loading</h1>;
+        }
+        else {
+            return <div className="tc">
+                <Header />
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll><ErrorBoundary><CardList robots={this.filterRobots()} /></ErrorBoundary></Scroll>
+            </div>;
+        }
+    }
+}
+
+export default MainPage;
